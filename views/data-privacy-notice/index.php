f58685024bad1b8f131469b4d879bdc467545ebb
itskimeng <markkimsacluti10101996@gmail.com>
2023-10-27 11:01:43 +0800

    code ref

--- views/data-privacy-notice/index.php
+++ views/data-privacy-notice/index.php
@@ -1,15 +1,15 @@
 
 <div class="container-fluid">
-    <div class="row" style="padding: 100px;">
+    <div class="row" style="padding: 50px;">
         <div class="col-lg-8">
             <?php include 'privacy_notice.html.php'; ?>
         </div>
-        <div class="col-lg-4 text-white aos-init" style="margin-top: 20px;">
-            <!-- <div>
-                <a href="calabarzon.dilg.gov.ph"><img src="images/side_banner/regional_website.jpg" class="zoomable-image" /></a>
-                <a href="fas.calabarzon.dilg.gov.ph"><img src="images/side_banner/fas.jpg" class="zoomable-image" /></a>
-                <a href="intranet.calabarzon.dilg.gov.ph"><img src="images/side_banner/dilg_intranet.jpg" class="zoomable-image" /></a>
-            </div> -->  
+        <div class="col-lg-3 text-white aos-init" style="margin-top: 20px;">
+        <div>
+                <a href="https://calabarzon.dilg.gov.ph"><img src="images/side_banner/regional_website.jpg" class="zoomable-image" style="position: absolute; right: 0;" /></a>
+                <a href="https://fas.calabarzon.dilg.gov.ph"><img src="images/side_banner/fas.jpg" class="zoomable-image" style="position: absolute; right: 0;margin-top:400px;" /></a>
+                <a href="https://intranet.calabarzon.dilg.gov.ph"><img src="images/side_banner/dilg_intranet.jpg" class="zoomable-image" style="position: absolute; right: 0;margin-top:200px;" /></a>
+            </div> 
         </div>
     </div>
 </div>
